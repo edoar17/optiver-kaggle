a4f713546364f8e74d4f9698b6c7297649b1ba3d
edoar17 <edoarvega@gmail.com>
2021-09-16 03:47:21 -0600

    finished processing features of book and trade. still must process feature on full df and do model

--- optiver.py
+++ optiver.py
@@ -18,28 +18,45 @@ train.info()
 train.describe() #Check statistics
 
 train['stock_id'].value_counts().sort_index() # Rows per stock_id
-sns.distplot(train['stock_id'])
+sns.distplot(train['stock_id']).set_title('density of stock_ids')
 
-train['time_id'].value_counts().sort_index() # Rows per time_id
+train['time_id'].value_counts().sort_values() # Rows per time_id
 
 # Distribution of Target
 sns.distplot(train['target'], color = 'b', label = 'target distribution').set(
     title = 'Distribution of target volatility',
     xlabel='volatility target')
 
+def log_return(series):
+    return np.log(series).diff()
+
+def pct_change(series):
+    return series.pct_change()
+
+def realized_volatility(series):
+    return np.sqrt(np.sum(series**2))
+
 
 
 # Visualize most volatile stocks
-vol = train.groupby('stock_id').mean()
+vol = train.groupby('stock_id')['target'].apply(realized_volatility)
+vol = vol.to_frame().reset_index()
+vol2 = train.groupby('stock_id')['target'].mean()
+vol2 = vol2.to_frame().reset_index()
+sns.set_style('darkgrid')
+sns.set_context("paper")
+sns.scatterplot(x='stock_id', y='target', data=vol).set_title('volatility of "target" per stock_id')
+sns.scatterplot(x='stock_id', y='target', data=vol2).set_title('mean "target" per stock_id')
+
+
 vol_stocks = train.nlargest(1000, 'target').sort_values('time_id')
-sns.histplot(x='stock_id', data=vol_stocks, bins=50).set_title(
-    'frequency of time_id within 1000 largest volatilities')
+sns.histplot(x='stock_id', data=vol_stocks, bins=50).set_title('frequency of stock_id within 1000 largest volatilities')
+
 
 # Volatility clustering, times with most volatility
 vol_clust = train.nlargest(1000, 'target')
 vol_clust.value_counts('time_id') #Proves volatility clustering
-sns.histplot(x='time_id', data=vol_clust, bins=50).set_title(
-    'frequency of time_id within 1000 largest volatilities')
+sns.histplot(x='time_id', data=vol_clust, bins=50).set_title('frequency of time_id within 1000 largest volatilities')
 
 #Some stocks are more volatility as well as theres times that are more volatile than others.
 #Maybe the volatility of the change in volatility can work as a reference.
@@ -60,14 +77,9 @@ trade_0 =  pd.read_parquet('kaggle-download/trade_train.parquet/stock_id=0')
 def row_id(stock_id, time_id):
     return f'{int(stock_id)}-{int(time_id)}'
 
-def log_return(series):
-    return np.log(series).diff()
-
-def realized_volatility(series):
-    return np.sqrt(np.sum(series**2))
-
 
 def closest(df, window, price_column):
+    ### Algortihm to calculate past [window] seconds_in_buckets' log return
     #find which price to minus
     dif_indexes = []
     idx = 0
@@ -82,16 +94,18 @@ def closest(df, window, price_column):
                 dif_indexes.append(idx)
             elif abs(series[idx] - To_Find) < abs(series[idx+1]-To_Find):
                 dif_indexes.append(idx)
-            elif abs(series[idx+1] - To_Find) < abs(series[idx+2]-x):
+            elif abs(series[idx+1] - To_Find) < abs(series[idx+2]-To_Find):
                 dif_indexes.append(idx+1)
                 idx += 1
-            elif abs(series[idx+2] - To_Find) < abs(series[idx+3]-x):
+            elif abs(series[idx+2] - To_Find) < abs(series[idx+3]-To_Find):
                 dif_indexes.append(idx+2)
                 idx += 2
-            elif abs(series[idx+3] - To_Find) < abs(series[idx+4]-x):
+            elif abs(series[idx+3] - To_Find) < abs(series[idx+4]-To_Find):
                 dif_indexes.append(idx+3)
                 idx += 3
-    
+            else: 
+                dif_indexes.append(idx+4)
+                idx += 4
     # print(len(dif_indexes))
     
     indexes_df = df[df['seconds_in_bucket']>window]
@@ -104,97 +118,166 @@ def closest(df, window, price_column):
     df_with_indexes['price_to_minus'] = df_with_indexes['index_to_minus'].apply(lambda x: np.nan if pd.isnull(x) else df[price_column][x])
     
     #calculate window return
-    new_col_name = str(price_column) + '_' + str(window) + '_log_return' # window = 100
+    new_col_name = str(price_column) + '_' + str(window)# + '_log_return' # window = 100
     df_with_indexes[new_col_name] = np.log(df_with_indexes[price_column]) - np.log(df_with_indexes['price_to_minus'])
     return df_with_indexes[[new_col_name]] #windowed return
 
-## Stock LIQUIDITY
+## BOOK
 def book_calcs(df):
     # size
     df['ask_size'] = df['ask_size1'].add(df['ask_size2'])
     df['bid_size'] = df['bid_size1'].add(df['bid_size2'])
     df['size_spread'] = df['ask_size'].add(-df['bid_size']) #if negative, bid sz > ask sz
-    df['median_size'] = df['ask_size'].add(-df['bid_size'])/2
+    df['d_size_spread'] = abs(df['size_spread']).diff()
+    
     # price
     df['ask_price'] = (df['ask_price1']+df['ask_price2'])/2
+    df['d_ask_price'] = pct_change(df['ask_price'])
     df['bid_price'] = (df['bid_price1']+df['bid_price2'])/2
-    df['price_spread'] = df['ask_price1'].add(df['bid_price1'])
-    df['price_spread2'] = df['ask_price2'].add(df['bid_price2'])
+    df['d_bid_price'] = pct_change(df['bid_price'])
+    
+    df['price_spread'] = df['ask_size']-df['bid_price']
+    df['d_price_spread'] = pct_change(df[['price_spread']])
+    # df['price_spread1'] = df['ask_price1'].add(df['bid_price1'])
+    # df['price_spread2'] = df['ask_price2'].add(df['bid_price2'])
     df['bid_price_spread'] = df['bid_price1'] - df['bid_price2']
+    df['d_bid_price_spread'] = pct_change(df['bid_price_spread'])
     df['ask_price_spread'] = df['ask_price2'] - df['ask_price1']
+    df['d_ask_price_spread'] = pct_change(df['ask_price_spread'])
+    
     # wap
     df['wap1'] = ( df['ask_size1']*df['bid_price1'] + df['bid_size1']*df['ask_price1'] )/(df['ask_size1']+df['bid_size1'])
     df['wap2'] = ( df['ask_size2']*df['bid_price2'] + df['bid_size2']*df['ask_price2'] )/(df['ask_size2']+df['bid_size2'])
-    df['wap3'] = ( df['ask_size1']*df['ask_price1'] + df['bid_size1']*df['bid_price1'] )/(df['ask_size1']+df['bid_size1'])
-    df['wap4'] = ( df['ask_size2']*df['ask_price2'] + df['bid_size2']*df['bid_price2'] )/(df['ask_size2']+df['bid_size2']) 
+    # df['wap3'] = ( df['ask_size1']*df['ask_price1'] + df['bid_size1']*df['bid_price1'] )/(df['ask_size1']+df['bid_size1'])
+    # df['wap4'] = ( df['ask_size2']*df['ask_price2'] + df['bid_size2']*df['bid_price2'] )/(df['ask_size2']+df['bid_size2']) 
+    
     # wap returns
     df['wap1_ret'] = log_return(df['wap1'])
     df['wap2_ret'] = log_return(df['wap2'])
-    df['wap3_ret'] = log_return(df['wap3'])
-    df['wap4_ret'] = log_return(df['wap4'])
+    # df['wap3_ret'] = log_return(df['wap3'])
+    # df['wap4_ret'] = log_return(df['wap4'])
+    
     # wap vol
     df['wap1_vol'] = realized_volatility(df['wap1_ret'])
     df['wap2_vol'] = realized_volatility(df['wap2_ret'])
-    df['wap3_vol'] = realized_volatility(df['wap3_ret'])
-    df['wap4_vol'] = realized_volatility(df['wap4_ret'])
+    # df['wap3_vol'] = realized_volatility(df['wap3_ret'])
+    # df['wap4_vol'] = realized_volatility(df['wap4_ret'])
     
+    #Cumulative returns
+    windowed_returns = closest(df.reset_index(), 100, 'wap1')
+    df = df.reset_index().merge(windowed_returns, left_index=True, right_index=True)
     
-    def calculateWindowedReturns(df, price_column, window):
-        windowed_returns = pd.DataFrame()
-        for i in df['time_id'].unique():
-            working_df = df[df['time_id']==i].reset_index()
-            rets = closest(working_df, window=window, price_column=price_column)
-            if not windowed_returns.empty:
-                windowed_returns = windowed_returns.append(rets, ignore_index=True)
-            else:
-                windowed_returns=rets
-        return windowed_returns
-        
-            
-            # working_df = df[df['time_id']==time_id].reset_index()
-            # return closest(working_df, window=window, price_column=price_column)
+    windowed_returns = closest(df.reset_index(), 200, 'wap1')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
     
-    # returns = pd.DataFrame()
-    # for i in df['time_id'].unique():
-    #     # print(i)
-    #     rets = calculateWindowedReturns(df, price_column='wap1', window=100, time_id=i)
-    #     # print(i)
-    #     if not returns.empty:
-    #         returns = returns.append(rets, ignore_index=True)
-    #     else:
-    #         returns=rets
-            
-    df['wap1_100'] = calculateWindowedReturns(df, price_column='wap1', window=100)  
-    df['wap1_200'] = calculateWindowedReturns(df, price_column='wap1', window=200)           
-    df['wap1_300'] = calculateWindowedReturns(df, price_column='wap1', window=300)
-    df['wap1_400'] = calculateWindowedReturns(df, price_column='wap1', window=400)
-    df['wap1_500'] = calculateWindowedReturns(df, price_column='wap1', window=500)
+    windowed_returns = closest(df.reset_index(), 300, 'wap1')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
     
+    windowed_returns = closest(df.reset_index(), 400, 'wap1')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
+    
+    windowed_returns = closest(df.reset_index(), 500, 'wap1')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
+
+    # print(windowed_returns)
+    # print(df)
     return df
 
-
+start = datetime.datetime.now()
 book_example = book_0.groupby('time_id').apply(book_calcs)
-book_example.columns
+end = datetime.datetime.now()
+print(end-start)
+book_example = book_example.set_index(['index'], drop=True)
+# book_example.columns
+a = book_example.value_counts(['time_id']).to_frame(name='count')
+print(a)
 
-### VOLATILITY 
+
+def book_features_processing(book_df, columns_to_keep):
+    df = book_df[columns_to_keep_book]
+    df = df.groupby('time_id').agg(['min', 'max', 'mean', 'std', 'count'])
+    return df
+
+columns_to_keep_book = ['time_id', 'size_spread', 'd_size_spread', 
+                   'ask_price', 'bid_price', 'd_ask_price', 'd_bid_price', 
+                   'price_spread', 'd_price_spread',
+                   'bid_price_spread', 'd_bid_price_spread', 
+                   'ask_price_spread', 'd_ask_price_spread',
+                   'wap1_ret', 'wap2_ret',
+                   'wap1_100', 'wap1_200', 'wap1_300', 'wap1_400', 'wap1_500']
+features_book = book_features_processing(book_example, columns_to_keep=columns_to_keep_book)
+features_book = features_book.rename_axis(['feature', 'stats'], axis='columns')
+# features_book['freq_time_id'] = features_book.merge(a, on='time_id')
+desc = features_book.describe()
+
+book_05 = book_0[book_0['time_id']==5]
+book_05 = book_05.groupby('time_id').apply(book_calcs)
+book_05 = book_05.set_index(['index'], drop=True)
+
+### TRADE 
 trade_05 = trade_0[trade_0['time_id']==5]
         
 def trade_calcs(df):
-    df['vwap'] = (df['price']*df['size'])/df['size']
     df['price_returns'] = log_return(df['price'])
-    df['vwap_returns'] = log_return(df['vwap'])
-    df = df.dropna()
-    #get realized_volatility
+    df['d_size'] = df['size'].diff()
+    df['size_per_order'] = df['size']/df['order_count']
+    df['d_order_count'] = df['order_count'].diff()
+    # df = df.dropna()
+    
+    # Cumulative returns
+    windowed_returns = closest(df.reset_index(), 100, 'price')
+    df = df.reset_index().merge(windowed_returns, left_index=True, right_index=True)
+    
+    windowed_returns = closest(df.reset_index(), 200, 'price')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
+    
+    windowed_returns = closest(df.reset_index(), 300, 'price')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
+    
+    windowed_returns = closest(df.reset_index(), 400, 'price')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
+    
+    windowed_returns = closest(df.reset_index(), 500, 'price')
+    df = df.merge(windowed_returns, left_index=True, right_index=True)
+    
+    #Realized_volatility
     df['price_vol'] = realized_volatility(df.price_returns)
-    df['vwap_vol'] = realized_volatility(df.vwap_returns)
     return df
 
-trade_example = trade_0.groupby('time_id').apply(trade_calcs).reset_index(drop=True)
+start = datetime.datetime.now()
+trade_example = trade_0.groupby('time_id').apply(trade_calcs)
+end = datetime.datetime.now()
+print(end-start)
 
 #N of trades per time id
-a = trade_example.value_counts('time_id')
-sns.histplot(a, y='time_id', bins=1000)
+a = trade_example.value_counts('time_id').to_frame(name='count')
+print(a)
+# sns.histplot(trade_example['time_id'], bins=5000).set_title('frequency of time_id in trade of stock_id=0')
 # trade_example['time_id'].nunique()
+
+def trade_features_processing(trade_df, columns_to_keep):
+    df = trade_df[columns_to_keep]
+    df = df.groupby('time_id').agg(['min', 'max', 'mean', 'std', 'count'])
+    return df
+
+columns_to_keep_trade = ['size', 'd_size',
+    'order_count', 'd_order_count', 'size_per_order',
+    'price_100', 'price_200', 'price_300', 'price_400', 'price_500']
+features_trade = trade_features_processing(trade_example, columns_to_keep_trade)
+features_trade = features_trade.rename_axis(['feature', 'stats'], axis='columns')
+
+#### JOIN BOOK AND TRADE FEATURES
+full_features = features_trade.merge(features_book, left_index=True, right_index=True)
+
+
+### Regression lots of trades
+
+
+
+
+
+
+
 
 ## Join calculated volatilities with row ids
 trade_vol = trade_example.drop_duplicates('time_id').filter(regex=r'(vol|time)')
@@ -275,17 +358,8 @@ def realized_volatility_per_time_id(file_path, prediction_column_name):
 c = realized_volatility_per_time_id('kaggle-download/book_train.parquet/stock_id=60','pred')
 
 
-def log_return(prices):
-    return np.log(prices).diff()
-
-def realized_volatility(logrets):
-    return np.sqrt(np.sum(logrets**2))
-
-
-
-
-trade_example = pd.read_parquet('kaggle-download/trade_test.parquet/')
-book_example = pd.read_parquet('kaggle-download/book_test.parquet/')
+# trade_example = pd.read_parquet('kaggle-download/trade_test.parquet/')
+# book_example = pd.read_parquet('kaggle-download/book_test.parquet/')
 
 
 
